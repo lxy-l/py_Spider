870407ec479ba8003c41f73f7adebe58143bb7f3
Roy <62872566+lxy-l@users.noreply.github.com>
2020-06-02 17:38:17 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -10,19 +10,6 @@ import re
 
 baseURL = ''
 headers = {
-	"authority":"search.bidcenter.com.cn",
-	"method": "GET",
-	"path": "",
-	"scheme": "https",
-	"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-	"accept-encoding": "gzip,deflate,br",
-	"accept-language": "zh-CN,zh;q=0.9",
-	"referer": "",
-	"sec-fetch-mode":"navigate",
-	"sec-fetch-site":"same-origin",
-	"sec-fetch-user":"?1",
-	"upgrade-insecure-requests":"1",
-	"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3970.5 Safari/537.36"
 }
 
 dbConfig = {
